768ac644b5f58ec84dd262c59d599cc31faa379a
Jesus Glez <jesusgleztr94@gmail.com>
2022-07-01 11:00:21 -0500

    Ajuste en los mail

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Murmurante'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@casaamate.com'),
+        'name' => env('MAIL_FROM_NAME', 'Casa Amate'),
     ],
 
     /*
