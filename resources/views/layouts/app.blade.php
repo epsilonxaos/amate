53852bb8d83a6fe0bd3725341ea018ea168a6abc
Jesus Glez <jesusgleztr94@gmail.com>
2022-07-01 18:34:44 -0500

    Idioma en calendario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{App::getLocale()}}">
 <head>
     @include('include.metas')
     <link rel="stylesheet" href="{{asset('plugins/bootstrap/bootstrap.min.css')}}">
